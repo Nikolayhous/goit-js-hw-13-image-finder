e0fcf76d0d8405f97fa23e2b034dd5c94b502452
Николай Гребенюк <Yanukhous@ukr.net>
2021-08-18 21:53:26 +0300

    Установил библиотеку бутстрап, подключил кнопку загрузки и прописал скрипт скрытие и открытие кнопки загрузки

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,11 +2,12 @@ export default class apiService {
     constructor() {
 this.searchQuery = '';
 this.page = 1;
+this.per_page = 12;
     }
 
     fetchArticles() {
          
-        const API_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=22607866-39f004f28173cdb15c56bee0e`;
+        const API_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=${this.per_page}&key=22607866-39f004f28173cdb15c56bee0e`;
         return fetch(API_URL)
         .then(response => response.json())
         .then(({hits}) => {
--- src/js/toSearchImages.js
+++ src/js/toSearchImages.js
@@ -1,38 +1,50 @@
 import { refs } from './refs';
 import apiService from './apiService';
 import itemCardsImage from '../templates/image-card.hbs'
+import LoadMoreBtn from './load-more-btn';
 
 import { error } from '@pnotify/core';
 import '@pnotify/core/dist/BrightTheme.css';
 
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+});
 
-const newApiService = new apiService()
+const newApiService = new apiService();
+
 
 refs.searchForm.addEventListener('submit', onSearchImages);
-refs.btnLoad.addEventListener('click', onLoadMore);
+loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
 
 
 function onSearchImages(e) {
     e.preventDefault();
-    // clearInput()
+   
 newApiService.query = e.currentTarget.elements.query.value;
 if(newApiService.query === '') {
+    loadMoreBtn.show();
+    loadMoreBtn.disable();
     return error({
         text: 'Enter data to search for pictures. For example <cat>'
-    }); 
+    });  
 }
-
+    loadMoreBtn.show();
     newApiService.resetPage();
-    newApiService.fetchArticles().then(hits => {
-        clearInput()
-        appendArticlesMarkup(hits) 
-    });
-    
+    clearInput()
+    fetchArticles();
 };
 
-function onLoadMore() {
-    newApiService.fetchArticles().then(appendArticlesMarkup);
+
+function fetchArticles() {
+    loadMoreBtn.disable();
+       newApiService.fetchArticles().then(hits => {
+        appendArticlesMarkup(hits);
+        loadMoreBtn.enable();
+    });
 }
+
+
 
 function appendArticlesMarkup(hits) {
     refs.galleryList.insertAdjacentHTML('beforeend', itemCardsImage(hits));
