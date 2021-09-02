35cec97f8c79f3a0b9852ba039b0e5dcdf8ff5f2
Николай Гребенюк <Yanukhous@ukr.net>
2021-09-02 20:59:14 +0300

    Добавил настройки скрола

--- src/js/onClickScrollTop.js
+++ src/js/onClickScrollTop.js
@@ -16,7 +16,12 @@ function onVisibleBtn () {
     };
 
   function onClickScrollTop() {
-        window.scrollTo(0, 0);
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+
  }
 
 
